6cfa67fda2b5246fe2355fb66837c94d3038f48c
会跑的BUG <1150284791@qq.com>
2017-04-11 20:53:04 +0800

    Update login.js

--- js/login.js
+++ js/login.js
@@ -3,7 +3,8 @@ var btns = document.getElementById("btnbg");
 var input1 = document.getElementById("input1");
 var input2 = document.getElementById("input2");
 input1.onblur = function(){
-	  if(input1.value.length==0&&input2.value.length==0){
+	  if(input1.value.length==0
+	     ||input2.value.length==0){
 		btns.style.backgroundColor="#fff";
       }else{
 		btns.style.backgroundColor="blue";
